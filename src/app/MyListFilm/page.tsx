60a700734811c468f177cf0d75d4d0102e1d8333
havizh <havizhabdurrahman@gmail.com>
2024-01-01 20:29:35 +0700

    first commit

--- src/app/MyListFilm/page.tsx
+++ src/app/MyListFilm/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import AfterLogin from "@/assets/components/navAfterLogin/after";
+import React from "react";
+import { Container, Row } from "react-bootstrap";
+import styles from "./page.module.css";
 
 export default function MyListFilm() {
   return (
-    <div>
-      MyListFilm
-    </div>
-  )
+    <Container>
+      <Row>
+        <AfterLogin />
+      </Row>
+      <Row>
+        <h1 className={styles.title}>My List Film</h1>
+        <div className={styles["container-list"]}>
+          <img src="/tomAndJerry.png" alt="" className={styles.thumbnail} />
+          <img src="/theMeg.png" alt="" className={styles.thumbnail} />
+        </div>
+      </Row>
+    </Container>
+  );
 }
